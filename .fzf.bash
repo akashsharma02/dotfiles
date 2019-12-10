b386d35e8a314489d06424393c5187fb3667999d
Akash Sharma <akashsharma@pop-os.localdomain>
2019-12-10 14:03:11 -0500

    Update script

--- .fzf.bash
+++ .fzf.bash
@@ -10,7 +10,4 @@ fi
 
 # Key bindings
 # ------------
-export FZF_TMUX=1
-export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 source "/home/akashsharma/.fzf/shell/key-bindings.bash"
--- symlink.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.fzf.bash ~/.fzf.bash
