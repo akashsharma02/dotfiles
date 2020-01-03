2b4c6b1f3a5eeeaeff759ab2397b2e0288f9ceb9
Akash Sharma <akashsharma@cmu.edu>
2020-01-03 02:20:45 -0500

    bashrc link

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.fzf.bash ~/.fzf.bash
 ln -sf ~/dotfiles/nvim ~/.config/
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 
 # setup tmux plugin manager, if not yet installed
 if [ -d "~/.tmux/plugins/tpm" ]; then
